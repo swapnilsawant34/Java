86ec828accda2255852e6dede8224b41df6c3f44
swapnilsawant34 <168524049+swapnilsawant34@users.noreply.github.com>
2024-05-08 13:09:21 +0530

    Pattern

---
+++ Pattern/src/pattern_star/pattern.java
@@ -0,0 +1,24 @@
+package pattern_star;
+import java.util.*;
+public class pattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=6;i++)
+		{
+			for(int j=1;j<=6;j++)
+			{
+				if(i<=j)
+				{
+					System.out.print("* ");
+				}else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
